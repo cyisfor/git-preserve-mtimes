4b548910115d5f7dc60b7f5629af0cfa659a8a75
autocommit <autocommit>
2017-08-13 23:26:06 +0000

    auto 0 4 61

---
+++ src/note.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h> // abort, getenv
+#include <stdarg.h> // va_*
+
+
+static void note(const char* how, int hlen,
+								 const char* file, int flen,
+								 int line, const char* fmt, va_list arg) {
+	fwrite(how, hlen, 1, stderr);
+	fputc(' ',stderr);
+	fwrite(file, flen, 1, stderr);
+	fprintf(stderr,":%d ",line);
+	vfprintf(stderr,fmt,arg);
+	va_end(arg);
+	fputc('\n',stderr);
+}
+
+#define BODY(name) \
+	va_list arg;																													\
+	va_start(arg, fmt);																										\
+	note(name,sizeof(name)-1,file,flen,line,fmt,arg);											\
+
+#define HEAD(ident) void ident ## f(const char* file, int flen, int line, const char* fmt, ...)
+
+#define DEFINE(ident,name) HEAD(ident) { \
+		BODY(name)																																\
+}
+
+DEFINE(spam,"DEBUG");
+DEFINE(info,"INFO");
+DEFINE(warn,"WARN");
+
+HEAD(error) {
+	BODY("ERROR");
+	if(getenv("error_nonfatal")) return;
+	abort();
+}

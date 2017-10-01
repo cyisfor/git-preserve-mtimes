78dc9f30acc61d879b29c4a140b6f58677820f84
autocommit <autocommit>
2017-10-01 01:39:38 +0000

    auto 0 25 459

--- src/install.c
+++ src/install.c
@@ -1,4 +1,7 @@
 #define _GNU_SOURCE
+
+#include "mystring.h"
+
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
@@ -14,9 +17,28 @@ int main(int argc, char *argv[])
 	if(s) {
 		hlen = s - here + 1; // include the slash
 	}
-	fwrite(here,hlen,1,stdout);
-	putchar('\n');
-	puts(here);
+
+	chdir(".git/hooks/");
+	char temp[] = ".tmpXXXXXX";
+	int out = mkstemp(temp);
+	assert(out >= 0);
+	write(out,here,hlen);
+	write(out,LITLEN("store\n"));
+
+	ensure0(rename(temp,"pre-commit"));
+	close(out);
+	out = mkstemp(temp);
+	write(out,here,hlen);
+	write(out,LITLEN("restore\n"));
+	ensure0(rename(temp,"post-checkout"));
+
+	symlink("post-checkout","post-merge");
+	symlink("post-checkout","post-rewrite");
+	symlink("post-checkout","post-reset");
+	return 0;
+}
+	
+
 	
 	return 0;
 }
